74d8b5ec40a8d73b88d52cfa0f73ac9fc1d5ac82
nachodiaz <ignaciosergiodiaz@gmail.com>
2019-02-15 22:38:59 -0300

    testing changes

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express')
 
-const port = 5000
+const port = 3100
 app.listen(port, `Esta esuchando en el puerto ${port}`)
 
 const app = express()
 
-app.set('view engine', 'pug')
-
-app.get('/', (req, res)=>{
-    res.render('views/inicio', {
-        
-    })
+app.get('/' , (req, res)=>{
+    res.send('hola')
 })
