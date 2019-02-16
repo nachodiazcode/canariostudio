f759d091a8ed9ea2a9b440bcd71726c885c511cc
nachodiaz <ignaciosergiodiaz@gmail.com>
2019-02-15 22:46:26 -0300

    correction app

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
+const app = express()
+
+
 const port = 3100
 app.listen(port, `Esta esuchando en el puerto ${port}`)
 
-const app = express()
 
 app.get('/' , (req, res)=>{
     res.send('hola Mundo')
