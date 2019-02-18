e360f6d5f1f1a67e4516a856ffe939f5ac668ab9
nachodiaz <ignaciosergiodiaz@gmail.com>
2019-02-18 13:54:17 -0300

    update latest changes in index, scrollspy installed and config header principal

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const favicon = require('express-favicon');
+
 const path = require('path')
 
 const app = express()
 
 app.set('view engine', 'pug')
 
+
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/views', express.static(path.join(__dirname, 'views')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'public','css')));
+app.use('/js', express.static(path.join(__dirname, 'public','js')));
 
 app.get('/' , (req, res)=>{
     res.render('inicio',{})
@@ -19,6 +24,19 @@ app.get('/quienes-somos' , (req, res)=>{
     res.render('quienes-somos',{})
 })
 
+app.get('/skills' , (req, res)=>{
+    res.render('skills',{})
+})
+
+app.get('/portafolio' , (req, res)=>{
+    res.render('portafolio',{})
+})
+
+app.get('/contacto' , (req, res)=>{
+    res.render('contacto',{})
+})
+
 app.listen(9000, () => {
     console.log('Escuchando en el puerto ' + 9000)
 })
+
