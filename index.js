c303789db0e88716813d956ef2c3c2797273c3f7
nachodiaz <ignaciosergiodiaz@gmail.com>
2019-02-23 18:07:33 -0300

    add file app js

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ const app = express()
 app.set('view engine', 'pug')
 
 
-
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/views', express.static(path.join(__dirname, 'views')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'public','css')));
 app.use('/js', express.static(path.join(__dirname, 'public','js')));
+app.use('/js', express.static(path.join(__dirname, 'public','js','app.js')));
+
 
 app.get('/' , (req, res)=>{
-    res.render('index',{})
+    res.render('inicio',{})
 })
 
 app.get('/quienes-somos' , (req, res)=>{
